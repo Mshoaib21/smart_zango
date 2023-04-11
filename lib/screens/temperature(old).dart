4807956b7937e6f8e2b7ac0bc10ff58ba582b48a
smart zango <mmshoaib574@gmail.com>
2023-04-11 13:55:31 +0500

    hi

---
+++ lib/screens/temperature(old).dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+
+class Temp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        title: Text("Temperature",style: TextStyle(color: Colors.purple[900]),),
+
+        backgroundColor: Colors.purple[100],
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios,
+          size: 20,
+          color: Colors.black,),
+
+
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 20,),
+                    
+                    Text("You can check the room temperature...",
+                    style: TextStyle(
+                      fontSize: 20,
+                    color:Colors.red[700]),)
+                  ],
+                ),
+               Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/temp.jpeg") //add  an image 
+                  )
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 100),
+                  child: Row(
+                    children:<Widget>[
+
+                /*
+                Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/home.jpg") //add  an image to welcome page
+                  )
+                ),
+              ),
+                     */ 
+                     
+                    ],
+                  ),
+                
+
+                
+                ),
+              Column(
+                  children: <Widget>[
+                    Text("Room temperature is 28 celcius",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                    SizedBox(height: 20,),
+                    
+                   
+                  ],
+                ),
+
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
+
+// we will be creating a widget for icons
+ /*Widget buildNormalIcons() => GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        children: [
+          IconButton(
+            iconSize: 80,
+            color: Colors.blue,
+            icon: Icon(Icons.videocam),
+            onPressed: () => print('You can moniter the baby now'),
+          ),
+         
+
+          IconButton(
+            iconSize: 80,
+            color: Colors.yellow,
+            icon: Icon(Icons.music_note),
+            onPressed: () => print('Play Music'),
+          ),
+
+          IconButton(
+            iconSize: 80,
+            color: Colors.green,
+            icon: Icon(Icons.crib),
+            onPressed: () => print('Swing the cardle'),
+        
+          ),
+        
+          IconButton(
+            iconSize: 80,
+            color: Colors.red,
+            icon: Icon(Icons.thermostat),
+            onPressed: () => print('Check the temperature'),
+          ),
+        
+          IconButton(
+            iconSize: 80,
+            color: Colors.blue,
+            icon: Icon(Icons.videocam),
+            onPressed: () => print('You can moniter the baby now'),
+          ),
+        
+        
+        
+        
+        
+        ],
+      );*/
